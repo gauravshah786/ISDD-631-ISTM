0d06445f607356904df60103163c6bc9b92229c1
Gaurav Shah <gauravshah_786@yahoo.com>
2018-04-21 11:59:50 -0500

    Corrected logout link and shopping cart

--- public/js/header.js
+++ public/js/header.js
@@ -1,21 +1,20 @@
 const user = JSON.parse(localStorage.getItem('user'));
 if(!user){
     window.location.href = '/login';
-} else {
-  $("#logout").click(event => {
-    event.preventDefault();
-    firebase.auth().signOut().then(() => {
-      window.location.href="/login";
-    }).catch(error => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-        alert(errorMessage);
-      });
-  });
-
-  $("#shc-side-open").click(event => {
-    event.preventDefault();
-      const href = "/shopping-cart?uid="+user.uid;
-      window.location = href;
-  });
 }
+$("#logout").click(event => {
+  event.preventDefault();
+  firebase.auth().signOut().then(() => {
+    window.location.href="/login";
+  }).catch(error => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+      alert(errorMessage);
+    });
+});
+
+$("#shc-side-open").click(event => {
+  event.preventDefault();
+  const href = "/shopping-cart?uid="+user.uid;
+  window.location = href;
+});
